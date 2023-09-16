45aff502e7fa43864f5cf9ca632116c82d9a040c
hansanhha <kwixs35@gmail.com>
2023-09-16 23:15:13 +0900

    initial commit

---
+++ src/main/java/com/know_wave/comma/comma_backend/CommaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.know_wave.comma.comma_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/know_wave/comma/comma_backend/CommaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.know_wave.comma.comma_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
