27ce0005af2ea088da5158a28a32245a607e0cc3
hansanhha <kwixs35@gmail.com>
2024-01-06 15:28:12 +0900

    아두이노 주문 보증금 결제(결제 승인, 취소) 예외 처리 코드 추가

--- src/main/java/know_wave/comma/arduino/order/service/DepositPaymentCallbackHandler.java
+++ src/main/java/know_wave/comma/arduino/order/service/DepositPaymentCallbackHandler.java
@@ -33,6 +33,8 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class DepositPaymentCallbackHandler implements PaymentCallbackHandler {
 
+    // 가상 번호
+    private static final int KAKAOPAY_FAILURE_CODE = -1039930293;
     private final ArduinoOrderNotification orderNotification;
     private final OrderRepository orderRepository;
     private final OrderDetailRepository orderDetailRepository;
@@ -83,6 +85,31 @@ public class DepositPaymentCallbackHandler implements PaymentCallbackHandler {
                 DEPOSIT_STATUS, orderResponse.getDepositStatus().getStatus());
 
         return FailCallbackResponse.create(response);
+    }
+
+    @Override
+    public void error(ErrorCallback errorCallback) {
+        int errorCode = errorCallback.getErrorCode();
+
+        OrderCallbackResponse orderCallbackResponse;
+        OrderNotificationRequest orderNotificationRequest;
+
+        if (errorCode == KAKAOPAY_FAILURE_CODE) {
+            orderCallbackResponse = failOrder(errorCallback.getOrderNumber(), OrderStatus.FAILURE_CAUSE_DEPOSIT_FAILURE);
+
+            orderNotificationRequest = OrderNotificationRequest.create(orderCallbackResponse.getOrderStatus(), orderCallbackResponse.getDepositStatus(),
+                            errorCallback.getOrderNumber(), errorCallback.getAccountId());
+
+            orderNotification.notify(orderNotificationRequest);
+            return;
+        }
+
+        orderCallbackResponse = failOrder(errorCallback.getOrderNumber(), OrderStatus.FAILURE_CAUSE_SERVER);
+
+        orderNotificationRequest = OrderNotificationRequest.create(orderCallbackResponse.getOrderStatus(), orderCallbackResponse.getDepositStatus(),
+                        errorCallback.getOrderNumber(), errorCallback.getAccountId());
+
+        orderNotification.notify(orderNotificationRequest);
     }
 
     @Override
